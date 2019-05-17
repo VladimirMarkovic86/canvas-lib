f589adb24ce6e7db9d661754eaf86a9ab36575dd
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-05-17 17:45:21 +0200

    Task 63: Implemented tests for htmlcss-lib.core namespace.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/canvas-lib "0.1.0"
+(defproject org.clojars.vladimirmarkovic86/canvas-lib "0.1.1"
   :description "Canvas library"
   :url "http://github.com/VladimirMarkovic86/canvas-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.5"]
+                 [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.6"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.9"]
                  ]
 
