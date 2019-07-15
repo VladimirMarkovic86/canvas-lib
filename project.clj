c03956716ee478b180e2a041e6a344d5d43612d0
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 12:49:26 +0200

    Task 88: Removed public folder from tested clojurescript projects packed jar file.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject org.clojars.vladimirmarkovic86/canvas-lib "0.1.2"
+(defproject org.clojars.vladimirmarkovic86/canvas-lib "0.1.3"
   :description "Canvas library"
   :url "http://github.com/VladimirMarkovic86/canvas-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.6"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.10"]
+                 [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.7"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.11"]
                  ]
 
   :min-lein-version "2.0.0"
